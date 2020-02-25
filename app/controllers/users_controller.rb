181d8e3c10c8a22650a365d3c116961b95b6f78e
Fabian Vazquez <Fabianxvazquez@gmail.com>
2020-02-24 17:29:08 -0700

    set controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only [:show, :destroy]
+  before_action :set_user, only: [:show, :destroy]
 
   def index
     @users = User.all
@@ -10,6 +10,21 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
   end
 
   private
